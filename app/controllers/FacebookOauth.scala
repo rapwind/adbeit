090b9d005d07889d5207a8e898b035caa4efa9db
EATis <h.cod.ss@gmail.com>
2014-03-21 01:26:24 +0900

    facebook認証においてキャンセルを押したときのコールバックの修正

--- app/controllers/FacebookOauth.scala
+++ app/controllers/FacebookOauth.scala
@@ -1,5 +1,6 @@
 package controllers
 
+
 import play.api.Play
 import play.api.mvc._
 import play.api.libs.json._
@@ -46,11 +47,32 @@ object FacebookOauth extends Controller {
   */
 
   def signin = Action {
-    Redirect("https://graph.facebook.com/oauth/authorize?client_id=" + clientId + "&redirect_uri=" + redirectUri)
+    Redirect("https://graph.facebook.com/oauth/authorize?client_id=" + clientId + "&redirect_uri=" + redirectUri + "&scope=email,user_birthday,publish_stream")
   }
 
-  def callback(code: String) = Action.async {
+  def callback = Action { implicit request =>
+    request.queryString.get("code").flatMap(_.headOption).getOrElse("get_error") match {
+      case "get_error" => errorCallback
+      case _ => successCallback
+    }
+  }
 
+  def successCallback(implicit request: RequestHeader) = {
+    val code = request.queryString.get("code").flatMap(_.headOption).getOrElse("")
+    Ok("success code :" + code )
+  }
+
+  def errorCallback(implicit request: RequestHeader) = {
+    val error = request.queryString.get("error").flatMap(_.headOption).getOrElse("")
+    val error_code = request.queryString.get("error_code").flatMap(_.headOption).getOrElse("")
+    val error_description = request.queryString.get("error_description").flatMap(_.headOption).getOrElse("")
+    val error_reason = request.queryString.get("error_reason").flatMap(_.headOption).getOrElse("")
+    Ok("error:" + error + " error_code:" + error_code + " error_description:" + error_description + " error_reason:" + error_reason)
+  }
+
+
+/*
+  def callback(code: String) = Action.async {
     requestAccessToken(code) flatMap { response =>
 
       val accessToken = parseAccessToken(response.body)
@@ -61,7 +83,7 @@ object FacebookOauth extends Controller {
       userInfo map { response =>
         Logger.debug("postBody=" + response.json)
         Logger.debug("test future")
-        Redirect(routes.Projects.index)
+        Redirect(routes.Projects.index).withSession("email" -> "guillaume@sample.com")
       } recover {
         case e: java.net.ConnectException => Ok("失敗")
       }
@@ -71,7 +93,7 @@ object FacebookOauth extends Controller {
     }
 
   }
-
+*/
 
   def requestAccessToken(code: String) = WS.url("https://graph.facebook.com/oauth/access_token").post(Map(
     "client_id" -> Seq(clientId),
@@ -97,15 +119,77 @@ object FacebookOauth extends Controller {
     }
   }
 
-
-  /**
-  * SignOutメソッド
-  * sessionをflashingしてLoginメソッドにリダイレクト
-  */
-  def signout = Action {
-    Redirect(routes.FacebookOauth.signin).withNewSession.flashing(
-    "success" -> "You are now logged out"
-    )
-  }
-
 }
+
+
+/*
+{
+  "id": "100002019632417",
+  "name": "Hiroki Tanida",
+  "first_name": "Hiroki",
+  "last_name": "Tanida",
+  "link": "https://www.facebook.com/HIROKI.TANIDA",
+  "birthday": "09/24/1990",
+  "hometown": {
+    "id": "165080443539566",
+    "name": "Maibara-shi, Shiga, Japan"
+  },
+  "location": {
+    "id": "111937038818889",
+    "name": "Kusatsu-shi, Shiga, Japan"
+  },
+  "education": [
+    {
+      "school": {
+        "id": "435567356564987",
+        "name": "米原高等学校"
+      },
+      "year": {
+        "id": "136328419721520",
+        "name": "2009"
+      },
+      "type": "High School"
+    },
+    {
+      "school": {
+        "id": "112169502142922",
+        "name": "立命館大学(Ritsumeikan University)"
+      },
+      "year": {
+        "id": "138879996141011",
+        "name": "2013"
+      },
+      "type": "College"
+    },
+    {
+      "school": {
+        "id": "107604449269714",
+        "name": "Ritsumeikan University"
+      },
+      "year": {
+        "id": "120960561375312",
+        "name": "2013"
+      },
+      "type": "College"
+    },
+    {
+      "school": {
+        "id": "137929602988310",
+        "name": "立命館大学大学院"
+      },
+      "type": "Graduate School"
+    }
+  ],
+  "gender": "male",
+  "email": "kny.h-ss@ezweb.ne.jp",
+  "timezone": 9,
+  "locale": "ja_JP",
+  "verified": true,
+  "updated_time": "2013-12-24T07:15:57+0000",
+  "username": "HIROKI.TANIDA"
+}
+
+http://localhost:7070/callback?error=access_denied&error_code=200&error_description=Permissions+error&error_reason=user_denied#_=_
+
+GET /callback?code=AQBgFbuZExDx4CjoQUwqHf5AclFa8UWNpZ539_E-eRY8l8m2avtqIzJstZ0RnyqwklAMwg3ick-cKymIVeaP6P03Yk7-8ttFp0fjdXD2K7U1teph9LOC_KliwLYFRYLbqZQ0OfSV6YCOKAq7l5nSe2QdwI0V0OjV_jG5bkmysOirnAc6dJ9dzHHR2DoN8y1BFkf0JtWYPHBhNoyUdRBSPd8CPtOS977vkw9ubLgrEQylnf_u-wgBTo8amHrrA4adG03ECdVBnyU544vVcZ8U7kqCyOVAU4upsfctAM_qWeojemr2DNaS2-l5JFVQFILiewk
+*/
